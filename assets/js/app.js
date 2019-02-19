67549462f587adf3134d05e9bd70489cef3e0944
caijunhui <caijunhui@odianyun.com>
2019-02-19 10:33:55 +0800

    更新Keep

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,34 @@
 ---
 ---
 (function($,window){
-  $(function(){
-    var jf = new Firebase($CONFIG["firebase"]);
-  
-    jf.child("pv").on("value", function(data){
-  	var pv = data.val();
-  	var $pv = $("#pv");
-  	if($pv.size() > 0 && pv > 1){
-  		$pv.html(pv);
-  	}
-    });
-  
-    jf.child("details/" + $CONFIG['page']).on("value", function(data){
-  	var sp = data.val();
-  	var $sp = $("#sp");
-  	if($sp.size() >0 && sp > 1){
-  		$sp.html(sp);
-  	}
-    });
-  
-    jf.child("pv").transaction(function(pv){
-  	return (pv || 0) + 1;
-    });
-  
-    jf.child("details/" + $CONFIG["page"]).transaction(function(sp){
-  	return (sp || 0) + 1;
-    });
-  
-  });
+  // $(function(){
+  //   var jf = new Firebase($CONFIG["firebase"]);
+  //
+  //   jf.child("pv").on("value", function(data){
+  // 	var pv = data.val();
+  // 	var $pv = $("#pv");
+  // 	if($pv.size() > 0 && pv > 1){
+  // 		$pv.html(pv);
+  // 	}
+  //   });
+  //
+  //   jf.child("details/" + $CONFIG['page']).on("value", function(data){
+  // 	var sp = data.val();
+  // 	var $sp = $("#sp");
+  // 	if($sp.size() >0 && sp > 1){
+  // 		$sp.html(sp);
+  // 	}
+  //   });
+  //
+  //   jf.child("pv").transaction(function(pv){
+  // 	return (pv || 0) + 1;
+  //   });
+  //
+  //   jf.child("details/" + $CONFIG["page"]).transaction(function(sp){
+  // 	return (sp || 0) + 1;
+  //   });
+  //
+  // });
   // 等待网页中所有元素都完全加载到浏览器之后再执行.
   window.onload = function(){
     $('#container').masonry({
